fd6198d5dd05642e1cc7bd529a369210246db7b1
julien-monniot <n.ceska@gmail.com>
2016-02-26 16:48:46 +0100

    How to open/read binary file (just a stackoverflow copy/paste to be used later)

--- cipherLSB.py
+++ cipherLSB.py
@@ -7,4 +7,19 @@ def cipher_lsb_txt(input_file, carrier_file, output_file):
     print("# In cipher lsb text wrapper with parameters :"
           "\n\t-Input file : "+input_file+"\n\t-Carrier Image : "+carrier_file+"\n\t-Output File : "+output_file)
 
+
+    ### USEFUL (?) STUFFS (FOR LATER USE) ###
+    # Open input and carrier file :
+    in_f = open(input_file, "rb")
+    ca_f = open(carrier_file, "rb")
+
+    # Reading
+    try:
+        byte = in_f.read(1)
+        while byte != "":
+            # Do stuff with byte.
+            byte = in_f.read(1)
+    finally:
+        in_f.close()
+
     pass
